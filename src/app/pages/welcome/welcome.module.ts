cc2242cea37f3532ddde9e3891f0d94182f614cd
cw <userchenwei@gmail.com>
2021-03-17 16:06:36 +0800

    add CommonModule

--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 import { WelcomeRoutingModule } from './welcome-routing.module';
 
 import { WelcomeComponent } from './welcome.component';
+import {NzTableModule} from 'ng-zorro-antd/table';
+import {NzDividerModule} from 'ng-zorro-antd/divider';
+import {CommonModule} from '@angular/common';
 
 
 @NgModule({
-  imports: [WelcomeRoutingModule],
+  imports: [WelcomeRoutingModule, NzTableModule, NzDividerModule, CommonModule],
   declarations: [WelcomeComponent],
   exports: [WelcomeComponent]
 })
